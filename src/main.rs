bcb4073b867db91c032e645c563aaaca0d4ceba6
renghen <renghen@yahoo.com>
2024-01-03 12:48:18 +0400

    basic commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use clap::{Arg, Command};
+
+pub fn main() {
+    let command = Command::new("Dog Shelter sample Axum application")
+        .version("1.0")
+        .author("Renghen renghen@yahoo.com")
+        .about("Sample rust app using axum")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("configuration file location")
+                .default_value("config.json"),
+        );
+
+    let _command_matches = command.get_matches();
+
     println!("Hello, world!");
 }
