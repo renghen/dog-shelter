0341f75c012fa18754d0aedcd4596a84f5b48a3f
renghen <renghen@yahoo.com>
2024-01-07 21:06:25 +0400

    part 6 - migration into docker db

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,16 +3,20 @@ use anyhow::Ok;
 use clap::{ArgMatches, Command};
 
 mod hello;
+mod migrate;
 mod serve;
 
 pub fn configure(command: Command) -> Command {
     command
         .subcommand(hello::configure())
         .subcommand(serve::configure())
+        .subcommand(migrate::configure())
 }
 
 pub fn handle(matches: &ArgMatches, settings: &Settings) -> anyhow::Result<()> {
     hello::handle(matches)?;
     serve::handle(matches, settings)?;
+    migrate::handle(matches, settings)?;
+
     Ok(())
 }
