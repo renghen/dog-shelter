58b39285adf326620f63bcfa1a1a52d0e7062b95
renghen <renghen@yahoo.com>
2024-01-08 12:59:59 +0400

    part 6 - sea-orm setup

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,6 +2,7 @@ use crate::settings::Settings;
 use anyhow::Ok;
 use clap::{ArgMatches, Command};
 
+mod createadmin;
 mod hello;
 mod migrate;
 mod serve;
@@ -11,12 +12,13 @@ pub fn configure(command: Command) -> Command {
         .subcommand(hello::configure())
         .subcommand(serve::configure())
         .subcommand(migrate::configure())
+        .subcommand(createadmin::configure())
 }
 
 pub fn handle(matches: &ArgMatches, settings: &Settings) -> anyhow::Result<()> {
     hello::handle(matches)?;
     serve::handle(matches, settings)?;
     migrate::handle(matches, settings)?;
-
+    createadmin::handle(matches, settings)?;
     Ok(())
 }
