fcae6e7de7b016aada18669385b88c7cf11e6f59
renghen <renghen@yahoo.com>
2024-01-08 17:01:20 +0400

    part 8 done

--- src/api/handlers/login.rs
+++ src/api/handlers/login.rs
@@ -3,11 +3,24 @@ use crate::api::response::login::LoginResponse;
 use crate::api::response::TokenClaims;
 use crate::state::ApplicationState;
 
+use anyhow::{anyhow, Result};
+use argon2::Argon2;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::Json;
 use jsonwebtoken::{encode, EncodingKey, Header};
+use password_hash::{PasswordHash, PasswordVerifier};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use std::sync::Arc;
+
+fn validate_password(password: &str, hash: &str) -> Result<()> {
+    let argon2 = Argon2::default();
+    let parsed_hash = PasswordHash::new(hash).map_err(|e| anyhow!(e.to_string()))?;
+
+    argon2
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .map_err(|_e| anyhow!("Failed to verify password"))
+}
 
 pub async fn login(
     State(state): State<Arc<ApplicationState>>,
@@ -19,6 +32,25 @@ pub async fn login(
     let now = chrono::Utc::now();
     let iat = now.timestamp() as usize;
     let exp = (now + chrono::Duration::seconds(timeout)).timestamp() as usize;
+
+    match entity::user::Entity::find()
+        .filter(entity::user::Column::Username.eq(&payload.username))
+        .all(state.db_connections.load().as_ref())
+        .await
+    {
+        Ok(admins) => {
+            if admins.is_empty() {
+                return Err(StatusCode::UNAUTHORIZED);
+            }
+
+            let admin = &admins[0];
+            if validate_password(&payload.password, &admin.password).is_err() {
+                return Err(StatusCode::UNAUTHORIZED);
+            }
+        }
+        Err(_) => return Err(StatusCode::UNAUTHORIZED),
+    }
+
     let claims = TokenClaims {
         sub: payload.username,
         exp,
